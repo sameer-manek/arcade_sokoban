02d706d0c3d72cb8dd91f378198ee6114bf4916e
sameer-manek <sameermanek@hotmail.com>
2023-11-25 22:17:41 +0530

    added levels and implemented reload

--- script.js
+++ script.js
@@ -1,32 +1,38 @@
-const map = [
-    "########",
-    "#000000#",
-    "#P0BB00#",
-    "#000S00#",
-    "#000#00#",
-    "#000S00#",
-    "#000000#",
-    "########",
-]; // load level from levels.json file.
+var current_level = 0;
+
+fetch('./levels.json')
+.then(res => res.json())
+.then(json => {
+
+const levels = json;
+var map = levels[current_level];
+
+document.getElementById('current_level').innerHTML = current_level;
+document.getElementById('last_level').innerHTML = levels.length-1;
 
 const cell_types = {
+    "+": '<div class="cell player switch"></div>',
+    "*": '<div class="cell switch box"></div>',
     "#": '<div class="cell wall"></div>',
     "P": '<div class="cell player"></div>',
     "B": '<div class="cell box"></div>',
     "S": '<div class="cell switch"></div>',
-    "0": '<div class="cell"></div>'
+    "0": '<div class="cell"></div>',
 };
 
 var player = null; // denoted with P
 var boxes = []; // position of all the boxes on the map. denoted with B
 var switches = []; // position of all the switches on the map. denoted with S
 
-var END = false;
+var END = true;
 // NOTE: no of boxes and no of switches should be equal on the map.
 
 var grid = document.getElementById("grid");
 
 function draw_level() {
+    player = null;
+    boxes = [];
+    switches = [];
     let html = '';
 
     map.forEach((row, y) => {
@@ -35,15 +41,15 @@ function draw_level() {
         [...row].forEach((cell, x) => {
             html += cell_types[cell] || cell_types['0'];
 
-            if (cell === "P") {
+            if (cell === "P" || cell === "+") {
                 player = [y,x];
             }
 
-            if (cell === "B") {
+            if (cell === "B" || cell === "*") {
                 boxes.push([y,x]);
             }
 
-            if (cell === "S") {
+            if (cell === "S" || cell === "*" || cell === "+") {
                 switches.push([y,x]);
             }
         });
@@ -52,6 +58,14 @@ function draw_level() {
     });
 
     grid.innerHTML = html;
+    document.getElementById('message').innerHTML = `
+        <img src="./keys.png" alt="" height="100px">
+        <br>
+        <b>Use arrow keys to move & Space to reload level</b>
+    `;
+
+    console.log({player});
+    END = false;
 }
 
 // recursive check of objects to given position
@@ -115,6 +129,11 @@ function check_end() {
     return end;
 }
 
+function load_level () {
+    map = levels[current_level];
+    draw_level();
+}
+
 function move_player(v) { // move item on pos using velocity v
     let next_cell = add_matx([...player], v);
     let can_move = prepare_move(next_cell);
@@ -132,7 +151,15 @@ function move_player(v) { // move item on pos using velocity v
     // check if all the switches are pushed
     END = check_end()
     if (END) {
-        document.getElementById("message").innerHTML = "<h3>Puzzle Solved!</h3>"
+        if (current_level < levels.length - 1) {
+            document.getElementById("message").innerHTML = "<h3>Puzzle Solved!</h3><br/><p>Loading Next level in 2 seconds</p>";
+        
+            current_level++;
+
+            setTimeout(load_level, 2000);
+        } else {
+            document.getElementById("message").innerHTML = "<h3>Congrats! You solved all the puzzles!</h3>";
+        }
     }
 }
 
@@ -157,6 +184,12 @@ function keypress (key) {
         v = [0, 1];
     }
 
+    if (key === " ") {
+        // reload level
+        END = true;
+        load_level();
+    }
+
     move_player(v);
 }
 
@@ -167,3 +200,5 @@ function Init() {
 }
 
 Init();
+
+})
